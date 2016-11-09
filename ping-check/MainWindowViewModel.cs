6c60294fb1bcbd20ac34213c327511115aba4f5b
dukemiller <dukemiller928@hotmail.com>
2016-11-09 00:23:10 -0700

    Add text databinding for displaying ping

--- ping-check/MainWindowViewModel.cs
+++ ping-check/MainWindowViewModel.cs
@@ -5,6 +5,31 @@ namespace ping_check
 {
     internal sealed class MainWindowViewModel: INotifyPropertyChanged
     {
+        private const string Template = "Ping: {0}ms";
+
+        private string _text;
+
+        // 
+
+        public MainWindowViewModel()
+        {
+            Text = string.Format(Template, "??");
+        }
+
+        // 
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // 
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
