0ac45ccd6215e6a03a18252724cd571c9b950580
dukemiller <dukemiller928@hotmail.com>
2016-11-09 00:23:44 -0700

    Add business logic to gather and display ping

--- ping-check/MainWindowViewModel.cs
+++ ping-check/MainWindowViewModel.cs
@@ -1,20 +1,27 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace ping_check
 {
-    internal sealed class MainWindowViewModel: INotifyPropertyChanged
+    internal sealed class MainWindowViewModel : INotifyPropertyChanged
     {
         private const string Template = "Ping: {0}ms";
+
+        private readonly IPingService _ping;
 
         private string _text;
 
         // 
 
-        public MainWindowViewModel()
+        public MainWindowViewModel(IPingService ping)
         {
+            _ping = ping;
             Text = string.Format(Template, "??");
+            Main();
         }
+
+        public MainWindowViewModel() : this(new PingService()) {}
 
         // 
 
@@ -31,6 +38,15 @@ namespace ping_check
         // 
 
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public async void Main()
+        {
+            while (true)
+            {
+                Text = string.Format(Template, await _ping.GetPing());
+                await Task.Delay(2000);
+            }
+        }
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
