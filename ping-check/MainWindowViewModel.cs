ed75a610cfd25adfc0030a499f491d8c9fa27334
dukemiller <dukemiller928@hotmail.com>
2016-11-08 20:06:01 -0700

    Initial layout setup (very simple view/viewmodel/styling)

---
+++ ping-check/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ping_check
+{
+    internal sealed class MainWindowViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
